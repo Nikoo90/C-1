24d921f86fc7cd6a421361903f43210f110fae01
Nikoo90 <66570980+Nikoo90@users.noreply.github.com>
2022-09-16 12:01:00 +0300

    Merge min-max_3_numbers

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1, num2, num3;
+
+Console.Write("Enter one number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter two number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter fhree number: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"Max number-{num1}");
+}
+else if (num3 > num2)
+{
+    Console.WriteLine($"Max number-{num3}");
+}
+else
+{
+    Console.WriteLine($"Max number-{num2}");
+}
