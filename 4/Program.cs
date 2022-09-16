f19d36bf6a04950724ec1ffd3e06ece6a728196f
Nikoo90 <66570980+Nikoo90@users.noreply.github.com>
2022-09-16 12:35:49 +0300

    out put of even numbers

---
+++ 4/Program.cs
@@ -0,0 +1,9 @@
+﻿int num, count;
+Console.Write("Enret number: ");
+num = Convert.ToInt32(Console.ReadLine());
+count = 1;
+while (count <= num)
+{
+    if (count % 2 == 0) { Console.Write($"{count} "); }
+    count++;
+}
