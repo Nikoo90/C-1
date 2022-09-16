df5f52e7ca38bb0b48a557ffc0b164844850c059
Nikoo90 <66570980+Nikoo90@users.noreply.github.com>
2022-09-16 23:38:39 +0300

    Fixed a bug

--- 2/Program.cs
+++ 2/Program.cs
@@ -7,11 +7,11 @@ num2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter fhree number: ");
 num3 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2)
+if (num1 > num2 && num1 > num3)
 {
     Console.WriteLine($"Max number-{num1}");
 }
-else if (num3 > num2)
+else if (num3 > num2 && num3 > num1)
 {
     Console.WriteLine($"Max number-{num3}");
 }
