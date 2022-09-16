9fdcbc753eb70cb05727b0e536ab751acb66d039
Nikoo90 <66570980+Nikoo90@users.noreply.github.com>
2022-09-16 11:48:42 +0300

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,13 @@
+﻿int num1, num2;
+Console.Write("Enter one numbers: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter two numbers: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine($"Max number-{num1}, min number-{num2}");
+}
+else
+{
+    Console.WriteLine($"Max number-{num2}, min number-{num1}");
+}
